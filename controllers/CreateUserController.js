eb518920cefbf2e0d2e4cfa8e9f9e11dd008698c
kikstart2026-dev <poulomisaha588@gmail.com>
2026-04-24 14:44:54 +0530

    admin role management

--- controllers/CreateUserController.js
+++ controllers/CreateUserController.js
@@ -61,12 +61,23 @@ exports.createSubAdmin = async (req, res) => {
 
 exports.getAllSubAdmins = async (req, res) => {
   try {
-    const users = await User.find({ role: "subadmin" });
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+
+    const skip = (page - 1) * limit;
+
+    const users = await User.find({ role: "subadmin" })
+      .skip(skip)
+      .limit(limit);
+
+    const total = await User.countDocuments({ role: "subadmin" });
 
     res.status(200).json({
       success: true,
       data: users,
+      totalPages: Math.ceil(total / limit),
     });
+
   } catch (error) {
     res.status(500).json({
       success: false,
