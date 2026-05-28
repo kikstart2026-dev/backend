9d9c110dd3f627fbb282e3bf75915e6daeb05780
kikstart2026-dev <kikstart2026@gmail.com>
2026-05-28 12:27:32 +0530

    ---

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ const MessageRouter = require("./routes/Conversation/MessageRoute");
 //subscription
 const subscriptionRoutes = require( "./routes/Subscription/SubscriptionRoute");
 
+const subscriptionPaymentRoutes = require( "./routes/SubscriptionPayment/SubscriptionPaymentRoute" );
 
 
 
@@ -137,6 +138,8 @@ app.use("/api/v1",paymentRoutes);
 
 //sunscription
 app.use( "/api/v1/subscription",subscriptionRoutes);
+
+app.use( "/api/v1/subscription-payment", subscriptionPaymentRoutes);
 
 
 app.get("/api/v1", authMiddleware, (req, res) => {
