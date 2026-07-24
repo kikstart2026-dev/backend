765acf2da0e56db3e7fca85473faa06ec8587a70
kikstart2026-dev <poulomisaha588@gmail.com>
2026-07-24 21:38:29 +0530

    push all in main

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ mongoose
     console.error("❌ DB connection failed:", err.message);
   });
 
+  console.log("SMTP_HOST =", process.env.SMTP_HOST);
+console.log("SMTP_PORT =", process.env.SMTP_PORT);
+console.log("SMTP_USER =", process.env.SMTP_USER);
+console.log("MAIL_FROM =", process.env.MAIL_FROM);
+
+
 app.listen(port, () => {
   console.log(`✅ App is running at http://localhost:${port} ...`);
   // console.log("EMAIL =", process.env.EMAIL);
